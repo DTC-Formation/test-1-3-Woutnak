92c1585b626301dd959a61920209bee30530a4b7
Woutnak <Kantoramanandraibe01@gmail.com>
2023-07-11 11:36:20 +0300

    add modification

--- tp.py
+++ tp.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 #variable
 liste_taches = []
-
 tache = {
 
              "nom" :'',
@@ -12,23 +11,35 @@ tache = {
              "statut" : '',
 
             }
-
-# while True:
-print("Bienvenue dans le gestionnaire des tâches")
-print("1 - Ajout d'une tâche ")
-print("2 - Affichage des tâches")
-print("3 - Modification d'une tâche")
-print("4 - Enregistrement des tâches")
-
+utilisateurs = {}
+#///////LOGIN////////
+nom_utilisateur = input("Entrez votre nom s'il vous plait :")
+if nom_utilisateur in utilisateurs:
+      nom_utilisateur = input("Nom deja utilisé\nEntrez votre nom s'il vous plait:")
+elif nom_utilisateur not in utilisateurs:
+      utilisateurs['nom'] =  nom_utilisateur
+      print("Bonjour {} et bienvenue sur TO DO List" .format(utilisateurs['nom']))
+#il faut avoir un fichier json qui  enregistre tout les utilisateurs 
+print(utilisateurs)  
+#///////LES ACTIONS////////
+while True:
+   print("//////////////////////////////////////////")
+   print("Bienvenue dans le gestionnaire des tâches")
+   print("1 - Ajout d'une tâche ")
+   print("2 - Affichage des tâches")
+   print("3 - Modification d'une tâche")
+   print("4 - Enregistrement des tâches")
+   print("5 - Quitter TO DO List")
 # Ajout d'une tâche
-user_action = input("Entrer une action: ")
+   user_action = input("Entrer une action: ")
 
-while user_action != "1" and user_action != "2" and user_action != "3" and user_action != "4":
+   while user_action != "1" and user_action != "2" and user_action != "3" and user_action != "4" and user_action != "5":
         user_action = input("Action non reconnu\nEntrer une action: ")
 
+
 #1 - Ajout d'une tâche
-if user_action == "1":
-        # input nom, input
+   if user_action == "1":
+        # input nom, input deadline
         print("Veuillez ajouter une tâche ")
         input_nom = input("Entrer le nom: ")
         input_deadline = input("Entrer le deadline (AAAA-MM-JJ HH:MM): ")
@@ -36,29 +47,36 @@ if user_action == "1":
         #affection du dictionnaire tache
         tache['nom']  = input_nom
         tache['deadline'] = datetime
+        
         #conversion en json
-        data = json.dumps(tache)
+        #data = json.dumps(tache)
+        #fichier json   
+        f = open('tache.json',"r")
+        f.read()
+        
 
 #2 - Affichage des tâches
-elif user_action == "2" :
-#ajout de nouveau dans le liste des taches
-   liste_taches.append(tache)
-   print(tache)
-   print(liste_taches)
-   #fichier json
-   f = open('tache.json',"r")
-   f.read()
-
-#type de status :commencer,en cours,terminer,rater
-statut = ['commencer','en cours','terminer','rater']
-deadline = datetime.fromisoformat(input_deadline)
-delai = datetime.now - deadline
+   elif user_action == "2" :
+#ajout de nouveau dans le liste des tach#Ajout des taches dans liste_taches
+       liste_taches.append(tache['nom'])
+       print("{} Votre To do list {} :".format(user_actuel,liste_taches)) 
+   
+   
 #3 - Modification d'une tâche
-elif user_action == "3" :
- print('Modification d\'une tâche')
+   elif user_action == "3" :
+      print('Modification d\'une tâche:')
+#type de status :commencer,en cours,terminer,rater
+# statut = ['commencer','en cours','terminer','rater']
+#deadline = datetime.fromisoformat(input_deadline)
+# delai = datetime.now - deadline
 
 #4 - Enregistrement des tâches
-elif user_action == "4" :
-outfile.write(data)
+   elif user_action == "4" :
+     #outfile.write(data)
+     print('Enregistrement d\'une tâche:')
+#Quitter
+   elif user_action == "5":
+     break
+
 
 
